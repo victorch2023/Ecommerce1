e90b6f81234437b1fe04cfd4b27d7fb91d9ff3da
victorch2023 <chowwha@gmail.com>
2025-12-17 15:24:42 -0500

    Deploy frontend React to GitHub Pages

--- backend/src/routes/orders.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createOrder, getOrder, getUserOrders } from '../controllers/ordersController.js';
-const router = express.Router();
-
-router.post('/', createOrder);
-router.get('/my-orders', getUserOrders);
-router.get('/:id', getOrder);
-
-export default router;
